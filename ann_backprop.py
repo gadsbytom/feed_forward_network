fixed backprop bug

ann_backprop.py
@@ -22,45 +22,42 @@ def sigmoid_prime(x):
 
 # derivative of the tanh function
 def tanh_prime(x):
-    return 1-(np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))**2
+    return 1-((np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x)))**2
 
 # backpropagation once through
 def backprop(
-    input_weights, output_weights, output_hidden, ypred, ytrue, X_input, LR_O, LR_H, act='sigmoid'
+    input_weights, output_weights, output_hidden, ypred, ytrue, X_input, LR_O, LR_H, act
 ):
     # separate learning rates for outer and inner weights.
 
     wH = input_weights
     wO = output_weights
 
-    # STEP A:
+
     ytrue = ytrue.reshape(-1, 1)
     error = (ypred - ytrue) * log_loss(ytrue, ypred)  # although order doesn't matter
     # logging.debug(f'shape of error is {error.shape}')
 
-    # STEP B:
-    if act=='sigmoid':
-        act_deriv = sigmoid_prime(ypred)
-    elif act=='tanh':
-        act_deriv = tanh_prime(ypred)
+    # no option here as output layer is probability distribution
+    act_deriv = sigmoid_prime(ypred)
 
     # derivative of the sigmoid function with respect to the hidden output *
     y_grad = act_deriv * error
     # logging.debug(f'shape of ygrad is {y_grad.shape}')
 
-    # STEP C:
+
     hidden_out_with_bias = np.hstack(
         [output_hidden, np.ones((output_hidden.shape[0], 1))]
     )
     # logging.debug(f'shape of hidden_out_with_bias is {hidden_out_with_bias.shape}')
-    # don't forget the bias!
+    # include bias
     delta_wo = -np.dot(y_grad.transpose(), hidden_out_with_bias) * LR_O
     # logging.debug(f'shape of delta_wo is {delta_wo.shape}')
 
-    # and finally, old weights + delta weights -> new weights!
+    # old weights + delta weights -> new weights!
     wO_new = wO + delta_wo.transpose()
 
-    # STEP D:
+
     if act=='sigmoid':
         act_deriv_2 = sigmoid_prime(output_hidden)
     elif act=='tanh':
@@ -72,14 +69,14 @@ def backprop(
 
     # exclude the bias (3rd column) of the outer weights, since it is not backpropagated!
 
-    # STEP E:
+
     delta_wH = -np.dot(H_grad.transpose(), X_input) * LR_H
     wH_new = wH + delta_wH.transpose()  # old weights + delta weights -> new weights!
 
     return wH_new, wO_new
 
 
-def epoch(X, y, num_epochs, input_weights, output_weights, lr_o, lr_h, act='sigmoid'):
+def epoch(X, y, num_epochs, input_weights, output_weights, lr_o, lr_h, act):
     average_log_loss = []
     for i in range(num_epochs):
         hidden_output, ypred = feed_forward(X, input_weights, output_weights, act)

ann_feed_forward.py
@@ -13,7 +13,7 @@ def tanh(x):
     return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
 # feed forward the x values through the neurons
-def feed_forward(X, weights_input, weights_m, act='sigmoid'):
+def feed_forward(X, weights_input, weights_m, act):
 
     """Input: Observation data X and proceses one feed-forward loop
     Output: probablity distribution for y classes"""
@@ -24,8 +24,7 @@ def feed_forward(X, weights_input, weights_m, act='sigmoid'):
         act_y = tanh(weighted_x)
     act_y_bias = np.hstack([act_y, np.ones((act_y.shape[0], 1))])
     hidden_weighted_x = np.dot(act_y_bias, weights_m)
-    if act=='sigmoid':
-        final_y = sigmoid(hidden_weighted_x)
-    elif act=='tanh':
-        final_y = tanh(hidden_weighted_x)
+    
+    #no tanh cos activation is probablity distribution
+    final_y = sigmoid(hidden_weighted_x)
     return act_y, final_y